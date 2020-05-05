39bb2fd14533ff1336aa6c209162bd4b8efc7f0e
arntfield1978 <robert.arntfield+github@gmail.com>
2020-05-04 22:05:20 -0400

    Accidentally left confusion matrix out from last commit

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -56,6 +56,48 @@ def plot_roc(name, labels, predictions, class_id=1, dir_path=None):
     return plt
 
 
+def plot_confusion_matrix(labels, predictions, class_id=1, dir_path=None):
+    '''
+    Plot a confusion matrix for the ground truth labels and corresponding model predictions for a particular class.
+    :param labels: Ground truth labels
+    :param predictions: Model predictions
+    :param class_id: Index of class to consider
+    :param dir_path: Directory in which to save image
+    '''
+    plt.clf()
+    p = 1.0 / np.unique(labels).shape[0]
+    single_class_preds = predictions[:, class_id]    # Only care about one class
+    single_class_labels = (np.array(labels) == class_id) * 1.0
+    predictions = single_class_preds
+    labels = single_class_labels
+    ax = plt.subplot()
+    cm = confusion_matrix(labels, predictions > p)  # Calculate confusion matrix
+    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)  # Plot confusion matrix
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(yticks=[-0.5, 1.5], xticks=[0, 1], yticklabels=['0', '1'], xticklabels=['0', '1'])
+    ax.yaxis.set_major_locator(mpl.ticker.IndexLocator(base=1, offset=0.5))
+
+    # Print number of TPs, FPs, TNs, FNs on each quadrant in the plot
+    thresh = cm.max() / 2
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    # Set plot's title and axis names
+    plt.title('Confusion matrix p={:.2f}'.format(p))
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+
+    # Save the image
+    if dir_path is not None:
+        plt.savefig(dir_path + 'CM_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.png')
+
+    # Print these statistics
+    print('True (-)ves: ', cm[0][0], '\nFalse (+)ves: ', cm[0][1], '\nFalse (-)ves: ', cm[1][0], '\nTrue (+)ves: ',
+          cm[1][1])
+    return plt
+
+
 def visualize_heatmap(orig_img, heatmap, img_filename, label, probs, class_names, dir_path=None):
     '''
     Obtain a comparison of an original image and heatmap produced by Grad-CAM.
